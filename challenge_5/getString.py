8a8ecc2053dacde1ed53cf74264158e5f64310ea
p-karari <kararikelvin2002@gmail.com>
2023-10-07 16:44:24 +0300

    vhallenge 05

---
+++ challenge_5/getString.py
@@ -0,0 +1,11 @@
+# import string
+
+
+class getString():
+    def __init__(self):
+        self.string_ = input("Enter a string of your choice : ")
+    def get_string(self):
+        return self.string_
+    def print_str(self):
+
+        print(self.string_.upper())
---
+++ challenge_5/main.py
@@ -0,0 +1,15 @@
+# Question:
+# Define a class which has at least two methods:
+# getString: to get a string from console input
+# printString: to print the string in upper case.
+# Also please include simple test function to test the class methods.
+#
+# Hints:
+# Use __init__ method to construct some parameters
+
+from getString import getString
+
+str_obj = getString()
+str_obj.get_string()
+str_obj.print_str()
+
